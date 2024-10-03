72b64d20c6df12a12c90038ed2af1034391825c0
phamvana <72136536+phamvana@users.noreply.github.com>
2024-10-03 10:35:32 +0700

    cập nhật footer

--- js/breed.js
+++ js/breed.js
@@ -1,53 +1,61 @@
-'use strict';
-// alert("Xin chào, kiểm tra chương trình!")
+'use strict'; //https://developer.mozilla.org/en-US/docs/Glossary/Strict_mode
 /**
  * Button Add 
  * Giao diện ẩn phần thêm dữ liệu
  * Khi ấn Add thì mới hiện ra cho người nhập vào.
  */
-const addBreedbtn = document.getElementById("add__breed-btn");
-let showMain = false; 
-addBreedbtn.addEventListener('click', function(){
-    if(!showMain){
-        document.getElementById('main__breed').style.display='block';
-        showMain = !showMain;
-        addBreedbtn.innerHTML = "Hide";
-    }else{
-        document.getElementById('main__breed').style.display='none';
-        showMain = !showMain;
-        addBreedbtn.innerHTML = "Add";
+const addBreedbtn = document.getElementById("add__breed-btn"); //addBreedbtn = DOM có ID = add_breed-btn
+let showMain = false;   // Tạo và gán biến showMain = false
+addBreedbtn.addEventListener('click', function(){   //khi clck và button có ID add_breed-btn hàm sẽ thực hiện
+    if(!showMain){      // nếu showMain == true thì 
+        document.getElementById('main__breed').style.display='block';   //css cho DOM display là block ==> hiện
+        showMain = !showMain;   //thay đổi giá trị của biên showMain là ngược với showMain
+        addBreedbtn.innerHTML = "Hide";      //gán lại tên cho button là Hide
+    }else{ //ngược lại 
+        document.getElementById('main__breed').style.display='none'; //css cho DOM có ID = main__breed display là none
+        showMain = !showMain;   //đảo giá trị của showMain
+        addBreedbtn.innerHTML = "Add"; //Gán lại HTML cho Button là Add
     }
 
 })
 /**
- * Gán các giá trị dữ liệu từ form nhập
+ * Tạo và Gán các giá trị dữ liệu từ form nhập
  */
 const tableBodyEl = document.getElementById("tbody");               //tạo biến lưu đoạn code html
 const inputbreed = document.getElementById("input-breed");          // biến lưu DOM form inputbreed
 const inputbreedtype = document.getElementById("inputbreed-type");  //biến lưu DOM breedtype
 const submitbreedbtn = document.getElementById("submitbreed-btn");  // biến lưu DOM button submit
 /**
- * Thể hiện reder HTML bản Breed
+ * Thể hiện render HTML bản Breed
  */
 function renderTableBreed(breedArr){
-    tableBodyEl.innerHTML=""; //gán giá trị rỗng cho bảng.
+    tableBodyEl.innerHTML=""; //gán giá trị rỗng cho biến tableBodyEl.
     for(let i = 0; i < breedArr.length; i++){
         // alert(breedArr.length);
         // let breed = breedArr[i];
-        const row = document.createElement('tr');
-        row.innerHTML =`<tr>
+        const row = document.createElement('tr');           //gán giá trị row là phần tử <tr> của table
+        /**
+         * Thêm HTML khi vòng lập được thực hiện.
+         * Sử dụng "template literals" để đưa các giá trị biến vào vị trí thích hợp
+         * ES6 Javascript mới được bổ sung
+         * 
+         */
+        row.innerHTML =`<tr>                                    
 							<th scope="row">${i+1}</th>
 							<td>${breedArr[i].breed}</td>
 							<td>${breedArr[i].type}</td>
 							<td><button type="button" class="btn btn-danger" 
                             onclick="DeleteBreed('${breedArr[i].name}')"> <i class="bi bi-trash3"></i></button>
 							</td>
-						</tr>`;
-        tableBodyEl.appendChild(row);
-    }
-}
-
-renderTableBreed(breedArr); // render breed khởi tạo.
+						</tr>`;         //finish row.innerHTML
+        tableBodyEl.appendChild(row);   //appendChild(row) method
+    }//finish for
+} //finish functon
+/**
+ * Thực hiện hàm renderTableBreed
+ *  @param: breedArr
+ */
+renderTableBreed(breedArr); 
 /**
  * 1.Hàm kiểm tra namebreed có rỗng không
  * Nếu namebreed rỗng thì trả về false, ngược lại true
@@ -128,18 +136,15 @@ submitbreedbtn.addEventListener('click', function(e){
  * Nếu đồng ý thì xoá
  */
 const DeleteBreed = (breed)=>{
-    if(confirm('Bạn có muốn xoá không?')){
-        for(let i = 0; i < breedArr.length; i++){
-            if(breedArr[i].breed == breed){
-                alert("test");
-                // alert(`namebreed = ${namebreed}`);
-                breedArr.splice(i,1); //xoá 1 phần từ khỏi mảng
+    if(confirm('Bạn có muốn xoá không?')){          //nếu giá trị confirm = true thì
+        for(let i = 0; i < breedArr.length; i++){   //duyệt mảng breedArr 
+            if(breedArr[i].breed == breed){         //nếu tên trong mảng breedArr trùng với breed thì
+                breedArr.splice(i,1);               //xoá 1 phần từ khỏi mảng
                 saveToStorage('breedArr',breedArr);   // Lưu vào Storage
-                renderTableBreed(breedArr);  // Render mảng ra trang;
-                break;
-            }
-        }
-    }
+                renderTableBreed(breedArr);         // Render mảng ra trang;
+                break;                              //thoát vòng lập
+            }//finish if
+        } //finish for
+    } //finish if
 }
-
-
+//auth: Phạm Văn Á
